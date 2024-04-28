846239bd033fceb27aefa8e571937b4a5762eb66
parkhan1211 <bagkan1211@gmail.com>
2024-04-28 21:48:28 +0900

    refactor: improve code quality

--- src/components/main/Betting/index.tsx
+++ src/components/main/Betting/index.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { SubmitBox } from '../../../common';
-import { BettingBar } from '../../../common';
-import { BettingAmount } from '../../../common';
+import { SubmitBox, BettingAmount, BettingBar } from "../../../common";
 
-import * as S from './styled';
+import * as S from "./styled";
 
 export const Betting: React.FC = () => {
-    return (
-        <section>
-            <S.BettingSectionContainer>
-                <S.SituationBox>
-                    <S.ColorBox />
-                    예측 진행중
-                </S.SituationBox>
-                <S.ExplaneBox>얼마나 베팅하실 건가요?</S.ExplaneBox>
-                <S.BettingContainer>
-                    <BettingBar />
-                    <BettingAmount />
-                </S.BettingContainer>
-                <SubmitBox></SubmitBox>
-            </S.BettingSectionContainer>
-        </section>
-    );
+  return (
+    <section>
+      <S.BettingSectionContainer>
+        <S.SituationBox>
+          <S.ColorBox />
+          예측 진행중
+        </S.SituationBox>
+        <S.ExplaneBox>얼마나 베팅하실 건가요?</S.ExplaneBox>
+        <S.BettingContainer>
+          <BettingBar />
+          <BettingAmount />
+        </S.BettingContainer>
+        <SubmitBox />
+      </S.BettingSectionContainer>
+    </section>
+  );
 };
