fb00c00ff570b313c019746b0c21cd4dc4f451bf
imratatouille <130735410+imratatouille@users.noreply.github.com>
2024-04-30 19:39:13 +0900

    feat: add predict border styles

--- src/common/PredictBox/index.tsx
+++ src/common/PredictBox/index.tsx
@@ -1,36 +1,55 @@
-import React from "react";
-import * as S from "./styled";
-import { colors } from "../../styles";
+import React, { useState } from 'react';
+import * as S from './styled';
+import { colors } from '../../styles';
 
 export const PredictBox: React.FC = () => {
-  return (
-    <S.PredictContainer>
-      <S.LeftTeamBox>
-        <S.LeftDeptBox>보안과 2학년 1반</S.LeftDeptBox>
-        <S.LeftRatioBox>
-          <S.RatingBox color={colors.redTeamColor}>50%</S.RatingBox>
-          <S.RatingBar backgroundColor={colors.redTeamColor} />
-        </S.LeftRatioBox>
+    const [leftBoxClicked, setLeftBoxClicked] = useState(false);
+    const [rightBoxClicked, setRightBoxClicked] = useState(false);
 
-        <S.EtcBox>
-          <p>🎉1500</p>
-          <p>⛷️50</p>
-          <p>😢1:3.1</p>
-        </S.EtcBox>
-      </S.LeftTeamBox>
-      <S.RightTeamBox>
-        <S.RightDeptBox>보안과 2학년 2반</S.RightDeptBox>
-        <S.RightRatioBox>
-          <S.RatingBox color={colors.blueTeamColor}>50%</S.RatingBox>
-          <S.RatingBar backgroundColor={colors.blueTeamColor} />
-        </S.RightRatioBox>
+    const handleLeftBoxClick = () => {
+        setLeftBoxClicked(true);
+        setRightBoxClicked(false);
+    };
 
-        <S.EtcBox>
-          <p>🎉1500</p>
-          <p>⛷️50</p>
-          <p>😢1:3.1</p>
-        </S.EtcBox>
-      </S.RightTeamBox>
-    </S.PredictContainer>
-  );
+    const handleRightBoxClick = () => {
+        setLeftBoxClicked(false);
+        setRightBoxClicked(true);
+    };
+
+    return (
+        <S.PredictContainer>
+            <S.LeftTeamBox
+                onClick={handleLeftBoxClick}
+                style={{ borderColor: leftBoxClicked ? colors.redTeamColor : colors.defaultBorderColor }}
+            >
+                <S.LeftDeptBox>보안과 2학년 1반</S.LeftDeptBox>
+                <S.LeftRatioBox>
+                    <S.RatingBox color={colors.redTeamColor}>50%</S.RatingBox>
+                    <S.RatingBar backgroundColor={colors.redTeamColor} />
+                </S.LeftRatioBox>
+
+                <S.EtcBox>
+                    <p>🎉1500</p>
+                    <p>⛷️50</p>
+                    <p>😢1:3.1</p>
+                </S.EtcBox>
+            </S.LeftTeamBox>
+            <S.RightTeamBox
+                onClick={handleRightBoxClick}
+                style={{ borderColor: rightBoxClicked ? colors.blueTeamColor : colors.defaultBorderColor }}
+            >
+                <S.RightDeptBox>보안과 2학년 2반</S.RightDeptBox>
+                <S.RightRatioBox>
+                    <S.RatingBox color={colors.blueTeamColor}>50%</S.RatingBox>
+                    <S.RatingBar backgroundColor={colors.blueTeamColor} />
+                </S.RightRatioBox>
+
+                <S.EtcBox>
+                    <p>🎉1500</p>
+                    <p>⛷️50</p>
+                    <p>😢1:3.1</p>
+                </S.EtcBox>
+            </S.RightTeamBox>
+        </S.PredictContainer>
+    );
 };
--- src/common/PredictBox/styled.ts
+++ src/common/PredictBox/styled.ts
@@ -1,101 +1,101 @@
-import styled from "styled-components";
-import { colors } from "../../styles";
+import styled from 'styled-components';
+import { colors } from '../../styles';
 
 interface RatingBarProps {
-  backgroundColor: string;
+    backgroundColor: string;
 }
 
 interface RatingBoxProps {
-  color: string;
+    color: string;
 }
 
 export const PredictContainer = styled.div`
-  width: 100%;
-  height: 240px;
-  border-radius: 10px;
-  background-color: #ffffff;
-  margin-top: 15px;
-  font-size: 17px;
-  display: flex;
-  font-weight: bold;
-  padding: 0 10px;
-  gap: 15px;
-  justify-content: center;
+    width: 100%;
+    height: 240px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    margin-top: 15px;
+    font-size: 17px;
+    display: flex;
+    font-weight: bold;
+    padding: 0 10px;
+    gap: 15px;
+    justify-content: center;
 `;
 
 export const RightTeamBox = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: flex-start;
-  flex-direction: column;
-  gap: 10px;
-  border: 3px solid #e7e7e7;
-  border-radius: 20px;
-  padding: 10px 5px;
-  text-align: center;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 10px;
+    border: 3px solid #e7e7e7;
+    border-radius: 20px;
+    padding: 10px 5px;
+    text-align: center;
 `;
 export const LeftTeamBox = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex-direction: column;
-  align-items: flex-end;
-  gap: 10px;
-  border: 3px solid #e7e7e7;
-  border-radius: 20px;
-  padding: 10px 5px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 10px;
+    border: 3px solid #e7e7e7;
+    border-radius: 20px;
+    padding: 10px 5px;
 `;
 
 export const LeftRatioBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-  align-items: flex-end;
-  margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    align-items: flex-end;
+    margin: 0 auto;
 `;
 
 export const RightRatioBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-  align-items: flex-start;
-  margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    align-items: flex-start;
+    margin: 0 auto;
 `;
 
 export const LeftDeptBox = styled.p`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  font-size: 13px;
-  color: ${colors.redTeamColor};
-  margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    font-size: 13px;
+    color: ${colors.redTeamColor};
+    margin: 0 auto;
 `;
 export const RightDeptBox = styled.p`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  font-size: 13px;
-  font-weight: bold;
-  color: ${colors.blueTeamColor};
-  margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    font-size: 13px;
+    font-weight: bold;
+    color: ${colors.blueTeamColor};
+    margin: 0 auto;
 `;
 
 export const RatingBox = styled.div<RatingBoxProps>`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  font-size: 60px;
-  color: ${(props) => props.color};
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    font-size: 60px;
+    color: ${(props) => props.color};
 `;
 export const RatingBar = styled.div<RatingBarProps>`
-  background-color: ${(props) => props.backgroundColor};
-  border-radius: 100px;
-  width: 80px;
-  height: 13px;
+    background-color: ${(props) => props.backgroundColor};
+    border-radius: 100px;
+    width: 80px;
+    height: 13px;
 `;
 export const EtcBox = styled.p`
-  font-size: 20px;
-  font-weight: bold;
+    font-size: 20px;
+    font-weight: bold;
 `;
