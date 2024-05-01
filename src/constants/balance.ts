4d23cce72a4bdecb118b992ffa6aab64210a7982
parkhan1211 <bagkan1211@gmail.com>
2024-05-02 08:21:57 +0900

    feat: add dummyData

---
+++ src/constants/balance.ts
@@ -0,0 +1,8 @@
+export const Balance = {
+  message: "SUCCESS",
+  data: {
+    user_name: "차은우",
+    user_id: 1,
+    balance: 500,
+  },
+};
---
+++ src/constants/leaderBoard.ts
@@ -0,0 +1,11 @@
+export const LeaderBoard = {
+  message: "SUCCESS",
+  data: {
+    leaderboard: [
+      {
+        user_id: 1,
+        total_points: 2700,
+      },
+    ],
+  },
+};
