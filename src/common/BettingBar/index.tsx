75f76eb32aa7e2e2a153fd5e11e784ec43885afd
imratatouille <130735410+imratatouille@users.noreply.github.com>
2024-05-03 15:22:25 +0900

    fix: fix styles

--- src/common/BettingBar/index.tsx
+++ src/common/BettingBar/index.tsx
@@ -4,7 +4,7 @@ import * as S from './styled';
 export const BettingBar: React.FC = () => {
     return (
         <S.BettingContainer>
-            <S.BettingBox>베팅할 금액을 입력해주세요.</S.BettingBox>
+            <S.BettingBox type="number" placeholder="베팅할 금액을 입력해주세요." />
         </S.BettingContainer>
     );
 };
--- src/common/BettingBar/styled.ts
+++ src/common/BettingBar/styled.ts
@@ -1,26 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const BettingContainer = styled.div`
-  width: 100%;
-  height: 60px;
-  border-radius: 10px;
-  background-color: #ffffff;
-  margin-top: 15px;
-  font-size: 17px;
-  display: flex;
-  font-weight: bold;
-  padding: 0 10px;
-  gap: 15px;
+    width: 100%;
+    height: 60px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    margin-top: 15px;
+    font-size: 17px;
+    display: flex;
+    font-weight: bold;
+    padding: 0 10px;
+    gap: 15px;
 `;
 
-export const BettingBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background-color: #f5f5f5;
-  border-radius: 10px;
-  width: 100%;
-  height: 50px;
-  padding: 0 10px;
-  color: rgba(0, 0, 0, 0.5);
+export const BettingBox = styled.input.attrs({ type: 'number' })`
+    /* BettingBox의 스타일 */
+    width: 100%;
+    height: 50px;
+    padding: 0 10px;
+    color: ${(props) => (props.value ? 'rgba(0,0,0,0)' : 'rgba(0, 0, 0, 1)')};
+    border: none;
+    border-radius: 10px;
+    background-color: #f5f5f5;
+    font-size: 17px;
+    font-weight: midium;
+
+    /* placeholder의 스타일 */
+    &::placeholder {
+        color: rgba(0, 0, 0, 0.3);
+        content: '베팅할 금액을 입력해주세요';
+    }
 `;
