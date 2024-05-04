591fecb24239cdf0b9d80b48030ac3c4934edcfe
imratatouille <130735410+imratatouille@users.noreply.github.com>
2024-05-04 16:11:07 +0900

    feat: add allocation and percentage func

--- src/common/PredictBox/index.tsx
+++ src/common/PredictBox/index.tsx
@@ -1,64 +1,70 @@
 import React, { useState } from 'react';
 import * as S from './styled';
 import { colors } from '../../styles';
+import { detail } from '../../constants/detail';
 
 export interface PredictProps {
     dept: string;
     point: number;
     person: number;
     allocation: number;
+    percentage: number;
 }
 
-export const PredictBox: React.FC = () => {
-    const [leftBoxClicked, setLeftBoxClicked] = useState(false);
-    const [rightBoxClicked, setRightBoxClicked] = useState(false);
+const formatNumber = (value: number): string => {
+    if (value >= 1000000) {
+        return `${(value / 1000000).toFixed(1)}M`;
+    } else if (value >= 1000) {
+        return `${(value / 1000).toFixed(1)}K`;
+    } else {
+        return `${value}`;
+    }
+};
 
-    const handleLeftBoxClick = () => {
-        setLeftBoxClicked(true);
-        setRightBoxClicked(false);
-    };
+export const PredictBox: React.FC<PredictProps> = () => {
+    const [selectedTeam, setSelectedTeam] = useState<'team_a' | 'team_b' | null>(null);
+    const { team_a, team_b } = detail.data;
+    const total_people = team_a.total_people + team_b.total_people;
+    const team_a_percentage: string = ((team_a.total_people / total_people) * 100).toFixed(0);
+    const team_b_percentage: string = ((team_b.total_people / total_people) * 100).toFixed(0);
+    const team_a_allocation: string = (100 / +team_a_percentage).toFixed(1);
+    const team_b_allocation: string = (100 / +team_b_percentage).toFixed(1);
 
-    const handleRightBoxClick = () => {
-        setLeftBoxClicked(false);
-        setRightBoxClicked(true);
+    const handleTeamClick = (team: 'team_a' | 'team_b') => {
+        setSelectedTeam((prevTeam) => (prevTeam === team ? null : team));
     };
 
     return (
         <S.PredictContainer>
-            <S.TeamBox
-                isLeft={true}
-                onClick={handleLeftBoxClick}
-                style={{ borderColor: leftBoxClicked ? colors.redTeamColor : colors.defaultBorderColor }}
-            >
-                <S.DeptBox isLeft={true}>보안과 2학년 1반</S.DeptBox>
-                <S.RatioBox isLeft={true}>
-                    <S.RatingBox color={colors.redTeamColor}>50%</S.RatingBox>
-                    <S.RatingBar backgroundColor={colors.redTeamColor} />
-                </S.RatioBox>
+            {[team_a, team_b].map((team, index) => (
+                <S.TeamBox
+                    key={index}
+                    isLeft={index === 0}
+                    onClick={() => handleTeamClick(`team_${index === 0 ? 'a' : 'b'}` as 'team_a' | 'team_b')}
+                    style={{
+                        borderColor:
+                            selectedTeam === `team_${index === 0 ? 'a' : 'b'}`
+                                ? index === 0
+                                    ? colors.redTeamColor
+                                    : colors.blueTeamColor
+                                : colors.defaultBorderColor,
+                    }}
+                >
+                    <S.DeptBox isLeft={index === 0}>{team.team_id}</S.DeptBox>
+                    <S.RatioBox isLeft={index === 0}>
+                        <S.RatingBox color={index === 0 ? colors.redTeamColor : colors.blueTeamColor}>
+                            {index === 0 ? team_a_percentage : team_b_percentage}%
+                        </S.RatingBox>
+                        <S.RatingBar backgroundColor={index === 0 ? colors.redTeamColor : colors.blueTeamColor} />
+                    </S.RatioBox>
 
-                <S.EtcBox>
-                    <p>🎉1500</p>
-                    <p>⛷️50</p>
-                    <p>😢1:3.1</p>
-                </S.EtcBox>
-            </S.TeamBox>
-            <S.TeamBox
-                isLeft={false}
-                onClick={handleRightBoxClick}
-                style={{ borderColor: rightBoxClicked ? colors.blueTeamColor : colors.defaultBorderColor }}
-            >
-                <S.DeptBox isLeft={false}>보안과 2학년 2반</S.DeptBox>
-                <S.RatioBox isLeft={false}>
-                    <S.RatingBox color={colors.blueTeamColor}>50%</S.RatingBox>
-                    <S.RatingBar backgroundColor={colors.blueTeamColor} />
-                </S.RatioBox>
-
-                <S.EtcBox>
-                    <p>🎉1500</p>
-                    <p>⛷️50</p>
-                    <p>😢1:3.1</p>
-                </S.EtcBox>
-            </S.TeamBox>
+                    <S.EtcBox>
+                        <p>🎉{index === 0 ? formatNumber(team_a.total_point) : formatNumber(team_b.total_point)}</p>
+                        <p>⛷️{index === 0 ? team_a.total_people : team_b.total_people}</p>
+                        <p>😢1:{index === 0 ? team_a_allocation : team_b_allocation}</p>
+                    </S.EtcBox>
+                </S.TeamBox>
+            ))}
         </S.PredictContainer>
     );
 };
---
+++ src/constants/detail.ts
@@ -0,0 +1,15 @@
+export const detail = {
+    data: {
+        game_info: 'basketball',
+        team_a: {
+            team_id: '클라우드 보안과 2학년 1반',
+            total_point: 1321,
+            total_people: 32131,
+        },
+        team_b: {
+            team_id: '클라우드 보안과 2학년 2반',
+            total_point: 1237621,
+            total_people: 32131,
+        },
+    },
+};
