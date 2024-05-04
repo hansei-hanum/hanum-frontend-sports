b31b3ad10e51fed9cf5711be1482901e31a722ef
imratatouille <130735410+imratatouille@users.noreply.github.com>
2024-05-04 16:34:15 +0900

    refactor: refactoring RankList

--- src/components/RankList/index.tsx
+++ src/components/RankList/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
 import { RankBox } from '../../common/RankingBox';
+import { result } from '../../constants/result';
 
 import * as S from './styled';
 
 export const RankList: React.FC = () => {
+    // Convert object values to an array and sort by total points in descending order
+    const sortedUsers = Object.values(result.data).sort((a, b) => b.total_points - a.total_points);
+
+    const users = sortedUsers.map((userData, index) => (
+        <RankBox
+            key={index}
+            rank={index + 1}
+            name={userData.username}
+            point={userData.total_points}
+            dept={userData.dept} // You can update this to use userData.department if it's available
+        />
+    ));
+
     return (
         <section>
-            <S.RankSectionContainer>
-                <RankBox rank={1} name="권기현" point={1300} dept="클라우드보안과 2학년 1반"></RankBox>
-                <RankBox rank={2} name="아무개" point={333} dept="메타버스게임과 2학년 1반"></RankBox>
-                <RankBox rank={3} name="아무개2" point={222} dept="네트워크보안과 3학년 1반"></RankBox>
-                <RankBox rank={4} name="아무개3" point={111} dept="클라우드보안과 1학년 1반"></RankBox>
-            </S.RankSectionContainer>
+            <S.RankSectionContainer>{users}</S.RankSectionContainer>
         </section>
     );
 };
