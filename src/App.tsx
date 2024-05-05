17013ca1f66aceb75f71cae4ba462d730e25354c
imratatouille <130735410+imratatouille@users.noreply.github.com>
2024-05-05 16:02:49 +0900

    fix: change file route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from './components/layouts/AppLayout';
 import { Global } from '@emotion/react';
 import { colors, globalStyle } from './styles';
-import { Betting } from './components/main';
+import { Betting } from './components/Betting';
 
 function App() {
     return (
